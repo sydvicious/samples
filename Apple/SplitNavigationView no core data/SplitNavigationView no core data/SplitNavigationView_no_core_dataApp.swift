81003727179d0ba6c1bb4780d76b3102c2d2fc22
Syd Polk <sydpolk@gmail.com>
2022-07-20 09:35:26 -0500

    Adding SplitNavigationView with no core data from Xcode 14 beta

---
+++ Apple/SplitNavigationView no core data/SplitNavigationView no core data/SplitNavigationView_no_core_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitNavigationView_no_core_dataApp.swift
+//  SplitNavigationView no core data
+//
+//  Created by Syd Polk on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitNavigationView_no_core_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
